2d813e7588ff188da5b9bfdb412d94faa9be2e40
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2026-01-12 11:29:40 -0500

    built layout for cards and started with backend logic

--- memory/src/Container.jsx
+++ memory/src/Container.jsx
@@ -1,9 +1,19 @@
+import { useState } from 'react'
+import { Card } from './Card';
 import './Container.css'
 function Container(){
+    const [chosen, setChosen] = useState([]);
+    const [maxScore, setMaxScore] = useState(0);
+    const score = chosen.length
     return (
         <div className='Container-Main'>
-            <span>Score: </span>
-            <span>MaxScore: </span>
+            <div className='Score-Board'>
+                <span>Score: {score}</span>
+                <span>MaxScore: {maxScore}</span>
+            </div>
+            <div className='Container-Cards'>
+
+            </div>
         </div>
     )
 }
