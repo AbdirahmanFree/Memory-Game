dd09558db9e033133dec06e201e31f1c583b1d47
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2026-01-13 14:35:59 -0500

    edit

--- memory/src/Container.jsx
+++ memory/src/Container.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Card } from './Card';
 import './Container.css'
 const API_KEY = import.meta.env.VITE_API_KEY
@@ -7,8 +7,10 @@ function Container(){
     const [maxScore, setMaxScore] = useState(0);
     const [images, setImages] = useState([])
     const score = chosen.length
-    const img_ids= ['4IzOgM1bfOe6k','B29mTiqmDxDy0','WHVf9eG9nrujc3P6Ns', 'STJ7W3pc7F6iA','10Jg7krSvcCQes','qprVSR8zTojRe', 'pChYU23X2y8XC','3oKIPusXllLwBDGYBq','uTpY9ARfN2eqs','ptTfUd6PdJ0sM', 'IazYrqVvnaeeIi8W2Q','HVJJBHxqT4TOU', 'rC9e6sdnlqGqI', 'OoaTf8fEuesP6', 'wM2jsoKbVTur6']
+    const img_idsReal= ['4IzOgM1bfOe6k','B29mTiqmDxDy0','WHVf9eG9nrujc3P6Ns', 'STJ7W3pc7F6iA','10Jg7krSvcCQes','qprVSR8zTojRe', 'pChYU23X2y8XC','3oKIPusXllLwBDGYBq','uTpY9ARfN2eqs','ptTfUd6PdJ0sM', 'IazYrqVvnaeeIi8W2Q','HVJJBHxqT4TOU', 'rC9e6sdnlqGqI', 'OoaTf8fEuesP6', 'wM2jsoKbVTur6']
+    const img_ids = ['4IzOgM1bfOe6k', 'B29mTiqmDxDy0']
     const giphyUrl= "https://api.giphy.com/v1/gifs/"
+    const hasRun = useRef(false)
 
     function fisherYates(array){
         for(let i = array.length-1; i >0; i--){
@@ -19,35 +21,41 @@ function Container(){
         }
         return array
     }
-    console.log(fisherYates([1,2,3,4,5]))
     
-
+    
     useEffect(() => {
-        try {
-            img_ids.map(async (id) => {
-                const url = giphyUrl + id + '?api_key=' + API_KEY
-                const res = await fetch(url)
-                const data = await res.json()
-                //setImages(prev => prev.push(data.data.original.url))
-            })
-            
+            if(!hasRun.current) {
+                hasRun.current = true
+            async function fetchImages() {
+                try {
+                const requests = img_ids.map(async (id) => {
+                    const url = `${giphyUrl}${id}?api_key=${API_KEY}`
+                    const res = await fetch(url)
+                    const data = await res.json()
+                    setImages(prev => [...prev, data.data.images.original.url])
+                    
+                })
 
-        }
-        catch(error) {
-            console.log(error)
-        }
-        return () => {
+                
 
-        }
+                } catch (error) {
+                console.error(error)
+                }
+            }
 
-    }, [])
+            fetchImages()
+            }
+        }
+        
+        , []) 
     return (
         <div className='Container-Main'>
             <div className='Score-Board'>
                 <span>Score: {score}</span>
-                <span>MaxScore: {maxScore}</span>
+                <span>MaxScore: {images.length}</span>
             </div>
             <div className='Container-Cards'>
+                
 
             </div>
         </div>
